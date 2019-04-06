daa07cb994ca5431251236c56b24ee9611a5caa8
Lucy Griffiths <lgriffiths@homeaway.com>
2019-04-06 18:32:56 +0100

    Make the river better and SADNESS :'(

--- src/lambs/gameScreen.js
+++ src/lambs/gameScreen.js
@@ -1,23 +1,32 @@
 let house;
 let power_plant;
+let sad_house;
 
 const HOUSES = [];
+const SADNESS = 300;
 
 function setup(){
-  createCanvas(1000,600);
+  createCanvas(1250,700);
+
+  // Load the images to use
   house = loadImage('assets/house.png');
   power_plant = loadImage('assets/coal_power_plant.png');
+  sad_house = loadImage('assets/house_sad.png');
+
+  // Populate the coordinates of the houses
   for (i = 80; i <= 160; i+=80) {
-    for (j = 150; j<= 300; j+=75) {
+    for (j = 225; j<= 450; j+=75) {
       HOUSES.push([i, j]);
     }
+  }
+  for (i = 300; i <= 500; i += 100) {
+    HOUSES.push([i, 500]);
   }
   for (i = 600; i <= 800; i+=100) {
     for (j = 400; j<= 500; j+=100) {
       HOUSES.push([i, j]);
     }
   }
-  console.log(HOUSES);
 }
 
 function draw() {
@@ -31,17 +40,24 @@ function draw() {
 function drawRiver(){
   stroke(0, 0, 200);
   noFill();
-  bezier(30, 80, 600, 250, 400, 80, 800, 100);
-  bezier(25, 75, 595, 245, 395, 75, 795, 95);
+  for(i=0; i<20; i++){
+    bezier(0, 80+i, 600+i, 250+i, 400+i, 80+i, 1250, 100+i);
+  }
 }
 
 function drawHouses(){
   for (i = 0; i < HOUSES.length; i++){
-    image(house, HOUSES[i][0], HOUSES[i][1], house.width/5, house.height/5);
+    if(Math.hypot(HOUSES[i][0] - 385, HOUSES[i][1] - 385) <= SADNESS) {
+      image(sad_house, HOUSES[i][0], HOUSES[i][1], house.width/5, house.height/5);
+    } else {
+      image(house, HOUSES[i][0], HOUSES[i][1], house.width/5, house.height/5);
+    }
   }
 }
 
 function drawCoalMine(){
+  // console.log(power_plant.width/1.5)
+  // console.log(power_plant.height/1.5);
   image(power_plant, 300, 300, power_plant.width/1.5, power_plant.height/1.5);
 }
 
